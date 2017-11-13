8f72fd8f97154b8b932818db952c00d57d35f959
Azunyan- <panton123@gmail.com>
2017-11-13 16:37:39 +0100

    Fix .json, updated spec

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ function printCategories {
     //Import json
     //loop trough each ul and write out the contents of it including which cat it is
     //Suggest that we also directly style the category colours via the html which is written via this function and not the already prewritten css.
+    //If a list item has a colour and a hex write a style="" with that inside it for that list item so that each indivdual list item can actually be colour if the user so desires
 }
