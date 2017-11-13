81ebea5f1e400dc71e33e0b7d85f0f1b0773d2bb
Azunyan- <panton123@gmail.com>
2017-11-13 16:22:09 +0100

    Started prepwork for json

---
+++ main.js
@@ -0,0 +1,5 @@
+function printCategories {
+    //Import json
+    //loop trough each ul and write out the contents of it including which cat it is
+    //Suggest that we also directly style the category colours via the html which is written via this function and not the already prewritten css.
+}
