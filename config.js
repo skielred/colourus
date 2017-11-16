92331f8efc8bf840aeaffdf842c84756c706edcc
Azunyan- <panton123@gmail.com>
2017-11-16 06:01:02 +0100

    Json to js and start of js func

---
+++ config.js
@@ -0,0 +1,75 @@
+config =
+[
+  {
+    category: '1',
+    name: 'Search',
+    colour: '#e05555',
+    listItems: [
+      {
+        name: 'Google',
+        url: 'https://www.google.com/',
+        colour:'parent'
+      },
+      {
+        name: 'Bing',
+        url: 'https://www.bing.com/',
+        colour:'parent'
+      },
+      {
+        name: 'Yahoo!',
+        url: 'https://search.yahoo.com/',
+        colour:'parent'
+      },
+      {
+        name: 'DuckDuckGo',
+        url: 'https://www.duckduckgo.com/',
+        colour:'parent'
+      },
+      {
+        name: 'Baidu',
+        url: 'https://www.baidu.com/',
+        colour:'parent'
+      }
+    ]
+  },
+  {
+    category: '2',
+    name: 'Pingis',
+    colour: '#ff5555',
+    listItems: [
+      {
+        name: 'Google',
+        url: 'https://www.google.com/',
+        colour:'parent'
+      },
+      {
+        name: 'Bing',
+        url: 'https://www.bing.com/',
+        colour:'parent'
+      },
+      {
+        name: 'Yahoo!',
+        url: 'https://search.yahoo.com/',
+        colour:'parent'
+      },
+      {
+        name: 'DuckDuckGo',
+        url: 'https://www..com/',
+        colour:'parent'
+      },
+      {
+        name: 'Baidu',
+        url: 'https://www.baidu.com/',
+        colour:'parent'
+      }
+    ]
+  }
+];
+
+/*
+.cat1 { color: #e05555; }
+.cat2 { color: #e0b455; }
+.cat3 {	color: #9ae055; }
+.cat4 {	color: #55d4e0; }
+.cat5 { color: #8555e0; }
+*/
--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-function printCategories {
-    //Import json
+/*function printCategories {
     //loop trough each ul and write out the contents of it including which cat it is
     //Suggest that we also directly style the category colours via the html which is written via this function and not the already prewritten css.
     //If a list item has a colour and a hex write a style="" with that inside it for that list item so that each indivdual list item can actually be colour if the user so desires
+
+    
+} */
+function writeLists(){
+
+    config.forEach(function (element) { //Intial loop for the array of categories
+        //Intial ul and li header
+        //loop trough all the contents of the second array in the object
+        console.log(element);
+        document.write("<ul style='colour:" + element.colour + ";'>");
+        document.write("<li>" + element.name + "</li>");
+        document.write("</ul>");
+    }, this);
 }
+
